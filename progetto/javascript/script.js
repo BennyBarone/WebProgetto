afa556b7d65e28c298eaa529cf9925e07468bc2f
Benedetta Barone <benedetta.barone@studio.unibo.it>
2025-01-22 12:05:28 +0100

    speriamo bene

--- progetto/javascript/script.js
+++ progetto/javascript/script.js
@@ -1,4 +1,6 @@
 // Seleziona gli elementi
+
+// Seleziona gli elementi
 const menuButton = document.getElementById("menu");
 const overlay = document.getElementById("overlay");
 const closeButton = document.getElementById("closeBtn");
@@ -6,9 +8,21 @@ const closeButton = document.getElementById("closeBtn");
 // Mostra l'overlay al clic del menu
 menuButton.addEventListener("click", () => {
     overlay.classList.add("active");
+  
 });
 
 // Nascondi l'overlay al clic del pulsante di chiusura
 closeButton.addEventListener("click", () => {
+    
     overlay.classList.remove("active");
+});
+
+c
+
+// Nascondi l'overlay quando si clicca su "Home"
+const homeLink = document.getElementById("homeLink");
+
+homeLink.addEventListener("click", (event) => {
+    event.preventDefault(); // Impedisce il comportamento di default del link
+    location.reload(); // Ricarica la stessa pagina
 });
