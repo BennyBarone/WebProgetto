d066f9ffe5957291a0a2f631a497d71c82af9f71
Emily <emily.pedini@studio.unibo.it>
2025-01-22 14:15:36 +0100

    aggiunta gusti migliori

--- progetto/prodotti.php
+++ progetto/prodotti.php
@@ -3,6 +3,7 @@ require_once 'bootstrap.php';
 
 $templateParams["titolo"]="Nuvole di gelato -Vedi Prodotti";
 $templateParams["nome"]="vedi_prodotti.php";
+$templateParams["gustiMigliori"]=$dbh->getBestGusti();
 
 
 require 'template/base.php';
--- progetto/template/vedi_prodotti.php
+++ progetto/template/vedi_prodotti.php
@@ -1,2 +1,10 @@
-<h1 class="text-center mt-5">Prodotti in arrivo...</h1>
-<p class="text-center">Stiamo lavorando per mostrarti i nostri migliori prodotti.</p>
+<section>
+    <h2>I gusti più amati dai nostri clienti</h2>
+    <ul class="list-unstyled">
+        <?php foreach($templateParams["gustiMigliori"] as $gustiMigliori): ?>
+            <li>
+                <?php echo $gustiMigliori["Nome_gusto"];?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
