4d4eb2688a7793630129faaeefa3a760d8763ddd
Emily <emily.pedini@studio.unibo.it>
2025-01-22 12:02:37 +0100

    sistemato tutto

--- progetto/bootstrap.php
+++ progetto/bootstrap.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 define("UPLOAD_DIR", "./upload/"); //crea variabile
-//require_once("utils/functions.php"); //include solo una volta il file
+require_once("utils/function.php"); //include solo una volta il file
 require_once("db/database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "negozio_gelateria");
 
--- progetto/index.php
+++ progetto/index.php
@@ -2,6 +2,7 @@
 require_once 'bootstrap.php';
 
 $templateParams["titolo"] = "Nuvole di gelato - Home";
+$templateParams["nome"]="home.php";
 $templateParams["recensioni"]=$dbh->getRecensioni();
 
 require 'template/base.php';
