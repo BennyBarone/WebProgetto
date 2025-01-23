6ec92eff2586de43d44b2042f27e00730ca7a738
Benedetta Barone <benedetta.barone@studio.unibo.it>
2025-01-23 15:29:40 +0100

    aggiunto foto

--- progetto/template/vedi_contatti.php
+++ progetto/template/vedi_contatti.php
@@ -1,5 +1,7 @@
 <section>
-    <h1 classe="mt-2 ms-2"> Contatti</h1>
+    <h2 classe="pt-4 ps-4"> Contatti</h2>
+    <p class="mt-3 mb-1">Via Corso Giuseppe Garibaldi, 159, 61034 Fossombrone PU- Tel: 06 12345678</a></p>
+
 
 
 
