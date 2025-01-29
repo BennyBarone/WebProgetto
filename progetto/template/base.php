6548eb4b862667dcbc7deadd47769e7213404218
Benedetta Barone <benedetta.barone@studio.unibo.it>
2025-01-29 15:31:33 +0100

    okok

--- progetto/template/base.php
+++ progetto/template/base.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./css/style.css?v=123">
+    <link rel="stylesheet" type="text/css" href="./css/style.css?v=133">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
@@ -20,17 +20,13 @@
             <h1 class="text-center flex-grow-1">Nuvole di Gelato</h1>
               <!-- Menu a discesa -->
             <div class="dropdown">
-            <img src="img/profilo.png" alt="account"  class="clickable-image dropdown-toggle"  data-bs-toggle="dropdown" >
+            <img src="img/profilo.png" alt="account" id="profileDropdown" class="clickable-image dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <ul class="dropdown-menu dropdown-menu-end text-center">
            
-                <li><a href="#" class="dropdown-item">Accedi</a></li> <!-- Link alla pagina di login -->
+            <li><a href="#" class="dropdown-item">Accedi</a></li> <!-- Link alla pagina di login -->
                 
-<<<<<<< HEAD
-                <li><a href="iscrizione.php" class="dropdown-item">Registrati - in un attimo</a></li> <!-- Link alla pagina di registrazione -->
-=======
             <li><a href="iscrizione.php" class="dropdown-item" onclick="window.location.href='iscrizione.php'">Registrati - in un attimo</a></li>
             <!-- Link alla pagina di registrazione -->
->>>>>>> 1c901811cf4d40f2c74ae3201a9544e0d5c49595
                
                 <li><hr class="dropdown-divider"></li> <!-- Linea divisoria -->
               
